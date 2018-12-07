02a2a222d95874afb27e9ad7ab559029cab12efa
aman5prakash <amanprakash512@gmail.com>
2018-12-07 16:19:12 +0530

    pending games added1.5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,34 +7,36 @@ import * as signalR from '@aspnet/signalr';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
- 
+
   connection: any
   games: any
-  
+
   constructor() { }
 
   ngOnInit() {
     this.connection = new signalR.HubConnectionBuilder().withUrl('http://172.23.238.164:7000/gameplayhub').build();
     this.connection.start().then(() => this.connection.send("SendPendingGames")).catch((err) => console.log('Error::: ', err));
-    this.connection.on("GetPendingGames", (res) => {this.games = res
-    console.log("pending games", this.games);});
+    this.connection.on("GetPendingGames", (res) => {
+    this.games = res
+      console.log("pending games", this.games);
+    });
   }
 
-  gotoJoiningPage(GameId : string) {
+  gotoJoiningPage(GameId: string) {
     console.log(GameId);
-    // var game = this.games.Where(g=> g.gameId === GameId).FirstOrDefault();
-    var game = this.games.some(function (el) {
-      return el.gameId === GameId;
-    });
-    console.log(game);
-    if(game.NumberOfPlayersRequired == 2) {
-      window.location.href = "http://172.23.238.164:7000/gameplay/play/"+game.Topic+"/two-players";
-    }
-    else if(game.NumberOfPlayersRequired == 3) {
-      window.location.href = "http://172.23.238.164:7000/gameplay/play/"+game.Topic+"/three-players";
-    }
-    else {
-      window.location.href = "http://172.23.238.164:7000/gameplay/play/"+game.Topic+"/four-players";
+    for (let game of this.games) {
+      if (game.GameId === GameId) {
+        console.log(game);
+        if (game.NumberOfPlayersRequired == 2) {
+          window.location.href = "http://172.23.238.164:7000/gameplay/play/" + game.Topic + "/two-players";
+        }
+        else if (game.NumberOfPlayersRequired == 3) {
+          window.location.href = "http://172.23.238.164:7000/gameplay/play/" + game.Topic + "/three-players";
+        }
+        else {
+          window.location.href = "http://172.23.238.164:7000/gameplay/play/" + game.Topic + "/four-players";
+        }
+      }
     }
   }
 }
