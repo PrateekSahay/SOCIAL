e59543be09e52d7abbd908dbb20e12a46335a532
aman5prakash <amanprakash512@gmail.com>
2018-12-07 15:40:09 +0530

    pending games routing issues

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
   }
 
   gotoJoiningPage(GameId : string) {
-    var game = this.games.Where(g=> g.gameId==GameId).FirstOrDefault();
+    var game = this.games.Where(g=> g.gameId, GameId).FirstOrDefault();
     if(game.numberOfPlayersRequired == 2) {
       window.location.href = "http://172.23.238.164:7000/gameplay/play/"+game.topic+"/two-players";
     }
