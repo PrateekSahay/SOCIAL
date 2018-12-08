185e5acee302e0c9270dccf0789bd732b4353e76
aman5prakash <amanprakash512@gmail.com>
2018-12-08 17:24:12 +0530

    routing

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +11,26 @@ import { map } from 'rxjs/operators';
 })
 export class NavbarComponent {
 
+  userName: any
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private cookieService: CookieService
+  ) { }
+
+  ngOnInit() {
+    let token = this.cookieService.get("UserLoginAPItoken");
+    let jwtData = token.split('.')[1];
+    let decodedJwtJsonData = window.atob(jwtData);
+    let decodedJwtData = JSON.parse(decodedJwtJsonData);
+    let userName = decodedJwtData.Name;
+
+    this.userName = userName;
+  }
 
 }
